2c30bf06a4148e35e7ecb97988d780084d5770ae
newdana01 <newdana01@gmail.com>
2022-01-13 16:50:51 +0900

    [Modified] models.py - null=True 추가

--- students/dahee/users/models.py
+++ students/dahee/users/models.py
@@ -5,8 +5,8 @@ class User(models.Model):
     email = models.CharField(max_length=200, unique=True)
     password = models.CharField(max_length=400)
     phone_number = models.CharField(max_length=45)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     class Meta:
         db_table = 'users'
