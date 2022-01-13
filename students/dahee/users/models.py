5b0bf85eda81aae119702f6ccae1fe0f9514a4a9
newdana01 <newdana01@gmail.com>
2022-01-13 16:42:41 +0900

    [Modified] models.py

--- students/dahee/users/models.py
+++ students/dahee/users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=45)
-    email = models.CharField(max_length=200)
-    password = models.CharField(max_length=45)
+    email = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=400)
     phone_number = models.CharField(max_length=45)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
