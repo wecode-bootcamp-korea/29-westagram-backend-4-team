e47902896bb6bdc377a07e4257c91eeaf001e082
cch0807 <https://github.com/cch0807>
2022-01-13 17:07:19 +0900

    project modeling

--- students/changhyeon/users/models.py
+++ students/changhyeon/users/models.py
@@ -6,5 +6,8 @@ class Users(models.Model):
   email        = models.CharField(max_length=100, unique=True) 
   password     = models.CharField(max_length=100)
   phone_number = models.CharField(max_length=30, unique=True)
+  create_at    = models.DateTimeField(auto_now_add=True)
+  modified_at  = models.DateTimeField(auto_now=True)
+
   class Meta:
     db_table   = 'users'
