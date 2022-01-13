54e48896a9657efe9519a18a090ef7722ee33e0a
newdana01 <newdana01@gmail.com>
2022-01-13 16:58:56 +0900

    [Modified] models.py - null=True 추가

--- students/dahee/users/models.py
+++ students/dahee/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=45)
+    name = models.CharField(max_length=45, null=True)
     email = models.CharField(max_length=200, unique=True)
     password = models.CharField(max_length=400)
-    phone_number = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=45, null=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
