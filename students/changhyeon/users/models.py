5c050b387e5ab49e25e01ab039cbefd5eaef29e6
cch0807 <https://github.com/cch0807>
2022-01-13 17:09:01 +0900

    project modeling

--- students/changhyeon/users/models.py
+++ students/changhyeon/users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Users(models.Model):
   name         = models.CharField(max_length=30)
   email        = models.CharField(max_length=100, unique=True) 
