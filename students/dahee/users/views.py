424d48835e847ccdfae208e93bdf31c41d227591
newdana01 <newdana01@gmail.com>
2022-01-14 16:54:21 +0900

    [Modified] views.py 내 SignUpView 수정

--- students/dahee/users/views.py
+++ students/dahee/users/views.py
@@ -11,29 +11,29 @@ class SignUpView(View):
     def post(self, request):
         user_data = json.loads(request.body)
 
-        try:
+        try:          
+            REGEX_EMAIL    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            REGEX_PASSWORD = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&.]{8,}$'
+            REGEX_PHONE_NUMBER = '\d{3}-\d{3,4}-\d{4}'
+
+            if not re.match(REGEX_EMAIL, user_data["email"]):
+                return JsonResponse({"message": "INVALID_EMAIL"}, status=400)
+            if not re.match(REGEX_PASSWORD, user_data["password"]):
+                return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
+            if not re.match(REGEX_PHONE_NUMBER, user_data["phone_number"]):
+                return JsonResponse({"message": "INVALID_PHONE_NUMBER"}, status=400)
+
             user = User(
                 name = user_data["name"],
                 email = user_data["email"],
                 password = user_data["password"],
                 phone_number = user_data["phone_number"]
             )
-            
-            email_validation = re.compile(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$')
-            password_validation = re.compile(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&.]{8,}$')
-            phone_number_validation = re.compile(r'\d{3}-\d{3,4}-\d{4}')
-            
-            if not email_validation.search(user_data["email"]):
-                return JsonResponse({"message": "INVALID_EMAIL"}, status=400)
-            if not password_validation.search(user_data["password"]):
-                return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
-            if not phone_number_validation.search(user_data["phone_number"]):
-                return JsonResponse({"message": "INVALID_PHONE_NUMBER"}, status=400)
-                
+
             user.save()
+
+            return JsonResponse({"message": "SUCCESS"}, status=201)
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
         except IntegrityError:
             return JsonResponse({"message": "EMAIL_ALREADY_EXISTS"}, status=400)
-        else:
-            return JsonResponse({"message": "SUCCESS"}, status=201)
