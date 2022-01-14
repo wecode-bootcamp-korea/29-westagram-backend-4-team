0f782d3ceb298374a0fa5788b815545bf1cd3e6c
cch0807 <https://github.com/cch0807>
2022-01-14 15:35:23 +0900

    [MODIFY] views.py

--- students/changhyeon/users/views.py
+++ students/changhyeon/users/views.py
@@ -8,30 +8,32 @@ from django.core.exceptions import ValidationError
 
 class SignupView(View):
   def post(self, request):
-    data         = json.loads(request.body)
-    name         = data["name"]
-    email        = data["email"]
-    password     = data["password"]
-    phone_number = data["phone_number"]
+    try:
+      data         = json.loads(request.body)
+      name         = data["name"]
+      email        = data["email"]
+      password     = data["password"]
+      phone_number = data["phone_number"]
+      try: #이메일이나 패스워드가 전달되지 않을 경우
+        validate_email(email)
+        validate_password(password)
+      except ValidationError:
+        return JsonResponse({"message": "KEY_ERROR"}, status =400)
 
-    try: #이메일이나 패스워드가 전달되지 않을 경우
-      validate_email(email)
-      validate_password(password)
-    except ValidationError:
+      if Users.objects.filter(email=email).exists(): # 중복된 email이 존재할 때
+        return JsonResponse({"message": "ALREADY_EXISTS"}, status=400)
+
+      Users.objects.create(
+        name         = name,
+        email        = email,
+        password     = password,
+        phone_number = phone_number,
+        )
+      return JsonResponse({"message": "SUCCESS"}, status= 201)
+
+    except:
       return JsonResponse({"message": "KEY_ERROR"}, status =400)
 
-    if Users.objects.filter(email=email).exists(): # 중복된 email이 존재할 때
-      return JsonResponse({"message": "ALREADY_EXISTS"}, status=400)
-
-    Users.objects.create(
-      name         = name,
-      email        = email,
-      password     = password,
-      phone_number = phone_number,
-    )
-      
-    return JsonResponse({"message": "SUCCESS"}, status= 201)
- 
 
   def get(self,request):
     return JsonResponse({"message": "SUCCESS"}, status= 200)
