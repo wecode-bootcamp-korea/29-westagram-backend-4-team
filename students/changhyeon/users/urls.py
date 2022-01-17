087d71df87b4bb19b67a7af90f030505c0ad5fd0
cch0807 <https://github.com/cch0807>
2022-01-17 13:33:41 +0900

    [modify] urls.py

--- students/changhyeon/users/urls.py
+++ students/changhyeon/users/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from .views      import SignupView,SigninView
-
+from .views      import SignupView
 urlpatterns = {
   path('signup', SignupView.as_view()),
-  path("signin", SigninView.as_view()),
 }
