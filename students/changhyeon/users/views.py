8694fc8296039967c9e8cedf7c07d3121d417d54
cch0807 <https://github.com/cch0807>
2022-01-17 14:21:29 +0900

    [modify] views.py

--- students/changhyeon/users/views.py
+++ students/changhyeon/users/views.py
@@ -2,13 +2,13 @@ import json
 from django.shortcuts       import render
 from django.http            import JsonResponse
 from django.views           import View
+from django.core.exceptions import ValidationError 
 from .validate              import validate_email, validate_password
 from users.models           import Users
-from django.core.exceptions import ValidationError 
 
 class SignupView(View):
   def post(self,request):
-    data = json.loads(request.body) #python dictionary
+    data = json.loads(request.body) 
 
     try:
       name         = data["name"]
@@ -16,15 +16,15 @@ class SignupView(View):
       password     = data["password"]
       phone_number = data["phone_number"]
 
-      validate_email(email) #이메일 검사
-      validate_password(password) #비밀번호 검사
+      validate_email(email)       
+      validate_password(password) 
 
       if Users.objects.filter(email = email).exists(): 
         return JsonResponse({"message": "EMAIL_AREADY_EXISTS"}, status=400)
 
       user = Users.objects.create(
-        name         =  name,
-        email        =  email,
+        name         = name,
+        email        = email,
         password     = password,
         phone_number = phone_number,
       )
@@ -35,5 +35,5 @@ class SignupView(View):
       return JsonResponse({"message": "KEY_ERROR"}, status=400)
 
     except ValidationError:
-      return JsonResponse({"message": "KEY_ERROR"}, status=400)
+      return JsonResponse({"message": "INVALID_ERROR"}, status=400)
 
