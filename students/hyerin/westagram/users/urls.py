56deee98ce0abc4c15bedbbed833d235edc19e9c
hyerin <hyeriniyam@gmail.com>
2022-01-18 15:52:31 +0900

    wip

--- students/hyerin/westagram/users/urls.py
+++ students/hyerin/westagram/users/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
-from users.views import SignupView, SigninView
 
 urlpatterns = [
-    # 127.0.0.1:8000/users
-    path('', SignupView.as_view()),
-    path('', SigninView.as_view()),
+    path('/signup', SignupView.as_view())
 ]
